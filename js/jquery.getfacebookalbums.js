3878d98cc2c447686202e310ce2493544309c227
Johannes Neumeier <johannes.neumeier@gmail.com>
2012-11-08 13:11:04 +0200

    - Restructured and renamed files - updated readme file - javascript code layout - defaults in english - added pickImageLabel option

--- js/jquery.getfacebookalbums.js
+++ js/jquery.getfacebookalbums.js
@@ -1,12 +1,11 @@
 /**
  * @projectDescription Load User Facebook Albums and Images with jQuery
  *
- * @version 1.0
- * @author David Brukarz
+ * @version 1.1
+ * @author David Brukarz, Johannes Neumeier
  *	
- * Example on http://www.davel.fr/demo/facebook/getFacebookAlbums.html
- * Details, in french, at http://www.davel.fr/techblog/2010/06/plugin-jquery-get-facebook-albums-photos
- * Date : 17/06/2010
+ * Details on original version, in french, at http://www.davel.fr/techblog/2010/06/plugin-jquery-get-facebook-albums-photos
+ * Date: 08/11/2012
  * Copyright ©2010 David Brukarz <http://davel.fr/>
  *
  * Released under the MIT licence:
@@ -14,65 +13,65 @@
  */
 
 
-(function($) {
+(function ($) {
 
-	var sessionVars;
 	var albumsData;
 	var photosData;
 	var selectedPhoto;
 	
-	var options = {
-					startConnectLabel:'Cliquez-ici pour vous connecter sur votre compte Facebook',
-					albumsLoadingLabel:'chargement des albums',
-					imagesLoadingLabel:'chargement des images',
-					needAuthorizeLabel:"Vous devez autoriser l'application.",
-					loadingImage:'images/loading.gif',
-					urlFacebookScript:'http://connect.facebook.net/fr_FR/all.js',
-					onImageSelected:null,
-					appId:null
-					}
+    var options = {
+        startConnectLabel  : 'Click to connect to Facebook',
+        albumsLoadingLabel : 'Loading albums',
+        imagesLoadingLabel : 'Loading images',
+        needAuthorizeLabel : 'Please grant access to the application',
+        pickImageLabel     : 'Pick image',
+        loadingImage       : 'images/loading.gif',
+        urlFacebookScript  : 'http://connect.facebook.net/en_US/all.js',
+        onImageSelected    : null,
+        appId              : null
+    }
 	
 
-	$.fn.getFacebookAlbums = function (pOptions) {
-		
-		$.extend(options,pOptions);
-		
-		if(options.appId == null){
-			$(this).html("The AppId is not set, try this code : $('#reference').getFacebookAlbums({appId:'YOUR-APP-ID'})");
+	$.fn.getFacebookAlbums = function (pOptions) {		
+		$.extend(options, pOptions);		
+		if (options.appId == null) {
+			$(this).html('The AppId is not set, try this code: ' +
+                '$(\'#reference\').getFacebookAlbums({appId:\'YOUR-APP-ID\'})');
 			return;
-		}
-		
+		}		
 		$.getScript(options.urlFacebookScript);
 		
 		$('body').prepend('<div id="fb-root"></div>');
-		$(this).html('<div id="fbListAlbumsContainer"><a href="#">'+options.startConnectLabel+'</a></div><div id="fbImagesContainer"></div><div id="fbPhotoSelection"></div>');
+		$(this).html('<div id="fbListAlbumsContainer"><a href="#">' + 
+		  options.startConnectLabel + '</a></div><div id="fbImagesContainer">' +
+		  '</div><div id="fbPhotoSelection"></div>');
 		
-		$("#fbListAlbumsContainer a").click(function(){
+		$("#fbListAlbumsContainer a").click(function () {
 			login();
 			return false;
 		})
 	}
 	
-	login = function(){
-		$('#fbListAlbumsContainer').html( options.albumsLoadingLabel + '<br><img src="'+options.loadingImage+'" />');
+	login = function () {
+		$('#fbListAlbumsContainer').html( options.albumsLoadingLabel + 
+		    '<br><img src="' + options.loadingImage + '" />');
 		FB.init({
 			appId: options.appId,
-			cookie : true,
+			cookie: true,
 			status: true
-		  });
-		  
-		//FB.getLoginStatus(function(response){console.log(response);});
+        });
 		
-		FB.login(function(response) {
-			if(response.status == "connected"){//response.scope && response.scope.indexOf('user_photos') != -1){
+		FB.login(function (response) {
+		    //response.scope && response.scope.indexOf('user_photos') != -1) {
+			if (response.status == 'connected') {
 				getAlbums();
-			 }else{
+			 } else {
 				$('#fbListAlbumsContainer').html(options.needAuthorizeLabel);
 			 }
 		 },{scope: 'user_photos'});
 	}
 	
-	getAlbums = function(){
+	getAlbums = function () {
 		FB.api(
           {
             method: 'photos.getAlbums'
@@ -81,22 +80,24 @@
         );		
 	}	
 
-	onAlbumsGot = function(data){
+	onAlbumsGot = function (data) {
 		var counter = 0;
 		var contentHTML = '<ul>';
 		albumsData = data;
-		for (var i = 0; i < data.length; i++){
+		for (var i = 0; i < data.length; i++) {
 			var album = data[i];
-			contentHTML += '<li class="fbAlbum" id="album_'+counter+'">'+album.name+'</li>';
-			counter++
+			contentHTML += '<li class="fbAlbum" id="album_' + counter + '">' + 
+			                 album.name + '</li>';
+			counter++;
 		}
 		contentHTML += '</ul>';
 		$('#fbListAlbumsContainer').html(contentHTML);
 		$('.fbAlbum').click(onFBAlbumSelected);
 	}
 	
-	onFBAlbumSelected = function (){
-		$('#fbImagesContainer').html(options.imagesLoadingLabel+'<br><img src="'+options.loadingImage+'">');
+	onFBAlbumSelected = function () {
+		$('#fbImagesContainer').html(options.imagesLoadingLabel + 
+            '<br><img src="' + options.loadingImage + '">');
 		var aid = albumsData[$(this).attr('id').replace('album_','')].aid;
 		FB.api(
           {
@@ -107,30 +108,31 @@
         );		
 	}
 	
-	onPhotosGot = function(data){
+	onPhotosGot = function (data) {
 		var counter = 0;
 		var contentHTML = '';
 		photosData = data;
-		for (var i = 0; i < data.length; i++){
+		for (var i = 0; i < data.length; i++) {
 			var photo = data[i];
-			contentHTML += '<div class="fbBlock" id="image_'+counter+'"><img src="'+photo.src+'"/></div>';
-			counter++
+			contentHTML += '<div class="fbBlock" id="image_' + counter + 
+			                 '"><img src="' + photo.src + '"/></div>';
+			counter++;
 		}
 		$('#fbImagesContainer').html(contentHTML);
 		$('.fbBlock').click(onFBPhotoSelected);
 	}
 	
-	onFBPhotoSelected = function (){
+	onFBPhotoSelected = function () {
 		// TODO set size
 		selectedPhoto = $(this).attr('id').replace('image_','');
-		contentHTML = '<p><img src="'+photosData[$(this).attr('id').replace('image_','')].src+'"></p><div id="fbValidatePhoto">Valider</div>';
+		contentHTML = '<p><img src="' + photosData[$(this).attr('id').replace('image_','')].src + 
+		              '"></p><div id="fbValidatePhoto">' + options.pickImageLabel + '</div>';
 		$('#fbPhotoSelection').html(contentHTML);
 		$('#fbValidatePhoto').click(finishValidatingPhoto);
 	}
 	
-	finishValidatingPhoto = function (){
+	finishValidatingPhoto = function () {
 		options.onImageSelected(photosData[selectedPhoto].src_big);
-	}
-	
+	}	
 	
 })(jQuery);
---
+++ readme.md
@@ -0,0 +1,43 @@
+# jQuery-GetFacebookAlbum plugin
+
+***
+
+[Original plugin and further instructions in french at](http://www.davel.fr/techblog/2010/06/plugin-jquery-get-facebook-albums-photos/)
+
+***
+
+## Quick use instructions:
+
+### 1. Create the facebook application
+Create a facebook app at https://developers.facebook.com/apps/ if you have not done so already. Activate the **Website with Facebook Login** section and enter the website url you will be using the plugin on. Look up the **App ID** and use this in your jquery code when initializing the plugin (see 4.)
+
+### 2. Include javascript and css links to your mark-up
+Remember to include a version of jQuery (1.4.2 or higher)
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js" type="text/javascript"></script>
+    <script src="js/jquery.getfacebookalbums.js" type="text/javascript"></script>
+    <link rel="stylesheet" href="css/jquery.getfacebookalbums.css" />
+
+### 3. Add a container that will hold the plugin dialog for fetching the photos
+    <div id="albumsContainer"></div>
+
+### 4. Lastly, throw the plugin on this container.
+    $('#albumsContainer').getFacebookAlbums({
+        appId: 'YOUR-APP-ID', 
+        onImageSelected: function (data) {
+            alert (data)
+        }
+        , ... further options like text strings
+    });
+
+#### Options available to configure the application to your needs: 
+* name: function (default value)
+* **appId***: String provided by Facebook in your app settings (null)
+* onImageSelected: Function with only one argument that returns the address of the image chosen by the user (null)
+* startConnectLabel: String label of the bond loguer and to begin the loading of the albums
+* albumsLoadingLabel: String message of waiting
+* imagesLoadingLabel: String message of waiting
+* needAuthorizeLabel: String error message if the user does not authorize the application to see his photographs
+* loadingImage: String way towards the image of preloading (images/loading.gif')
+* urlFacebookScript: String URL of the API Javascript of facebook (http://connect.facebook.net/en_US/all.js)
+
+\* mandatory!
